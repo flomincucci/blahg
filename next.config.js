d784b16d1bb98142b64a241b01ba9c83edfafc0d
Florencia Mincucci <florencia.mincucci@properati.com>
2021-05-14 16:15:33 -0300

    moving to blog theme

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 // next.config.js
-const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.js',
-});
-
+const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra({
     webpack(config) {
         config.plugins.pop(); // remove stork plugin which was added by nextra. Remove when nextra version is bumped!
