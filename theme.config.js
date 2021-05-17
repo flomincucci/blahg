de275ec4e8ef3a30092fe4703e5837a208d19822
Florencia Mincucci <florencia.mincucci@properati.com>
2021-05-17 12:35:32 -0300

    modify favicon

--- theme.config.js
+++ theme.config.js
@@ -18,23 +18,12 @@ export default {
       <meta name="description" content="Un blog sobre cosas" />
       <meta name="og:description" content="Un blog sobre cosas" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:image" content="https://blahg.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="blahg.vercel.app" />
       <meta name="twitter:url" content="https://blahg.vercel.app" />
       <meta name="og:title" content="Blahg: un blog sobre cosas" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="og:image" content="https://blahg.vercel.app/og.png" />
       <meta name="apple-mobile-web-app-title" content="Blahg" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
       <link
         rel="icon"
         type="image/png"
@@ -44,16 +33,10 @@ export default {
       <link
         rel="icon"
         type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/red-tea.png" />
     </>
   ),
   search: true,
